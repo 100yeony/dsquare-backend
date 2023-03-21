6888ff791c38d6d4eb6f44f4df805394a88f3061
이호열 <hoyeol.lee@kt.com>
2023-03-21 15:09:48 +0900

    Initial Commit

---
+++ src/test/java/com/ktds/dsquare/DSquareUserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.dsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSquareUserBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
