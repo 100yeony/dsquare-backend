c7e9fddbb652b7d634f9c8e9975d36236805c00b
NaKyoung-Kim <78343969+NaKyoung-Kim@users.noreply.github.com>
2023-04-13 23:43:12 +0900

    FIX: Change Response Type

--- src/main/java/com/ktds/dsquare/board/qna/controller/QuestionController.java
+++ src/main/java/com/ktds/dsquare/board/qna/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.ktds.dsquare.board.qna.controller;
 
 import com.ktds.dsquare.board.qna.domain.Question;
+import com.ktds.dsquare.board.qna.dto.BriefQuestionResponse;
 import com.ktds.dsquare.board.qna.dto.QuestionRequest;
 import com.ktds.dsquare.board.qna.dto.QuestionResponse;
 import com.ktds.dsquare.board.qna.service.QuestionService;
@@ -14,11 +15,11 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-//@RequestMapping("/board/questions")
 public class QuestionController {
 
     private final QuestionService questionService;
 
+    //create - 질문글 작성
     @PostMapping("/board/questions")
     public ResponseEntity<Void> createQuestion(@RequestBody QuestionRequest request){
         questionService.createQuestion(request);
@@ -27,15 +28,14 @@ public class QuestionController {
 
     //read - 질문글 전체 목록 조회
     @GetMapping("/board/questions")
-    public List<Question> getAllQuestions(@RequestParam Boolean workYn){
-        return questionService.getAllQuestions(workYn);
+    public ResponseEntity<List<BriefQuestionResponse>> getAllQuestions(@RequestParam Boolean workYn){
+        return new ResponseEntity<>(questionService.getAllQuestions(workYn), HttpStatus.OK);
     }
 
-
-    //read - 질문글&답변글 상세 조회
+    //read - 질문글 상세 조회
     @GetMapping("/board/questions/{qid}")
-    public QuestionResponse getQnADetail(@PathVariable("qid") Long qid) {
-        return questionService.getQuestionDetail(qid);
+    public ResponseEntity<QuestionResponse> getQnADetail(@PathVariable("qid") Long qid) {
+        return new ResponseEntity(questionService.getQuestionDetail(qid), HttpStatus.OK);
     }
 
     // 질문글 수정
@@ -51,8 +51,6 @@ public class QuestionController {
         questionService.deleteQuestion(qid);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
-
-
 
     //search - Q&A 검색(카테고리, 제목+내용, 사용자(이름))
     @GetMapping("/board/questions/search")
