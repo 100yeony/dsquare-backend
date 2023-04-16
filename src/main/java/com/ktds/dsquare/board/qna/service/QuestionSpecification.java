7d46017af55d585023b191fdfd3b8ac539f7aa6e
yeonhee baek <byh741@daum.net>
2023-04-16 13:41:59 +0000

    Revert "FIX: sesarch(add deleteYn, sort)"
    This reverts commit 0a35fc8052a453d8fe0593cfdab0ebeb09940a4f

--- src/main/java/com/ktds/dsquare/board/qna/service/QuestionSpecification.java
+++ src/main/java/com/ktds/dsquare/board/qna/service/QuestionSpecification.java
@@ -6,26 +6,23 @@ import org.springframework.data.jpa.domain.Specification;
 import javax.persistence.criteria.Predicate;
 
 public class QuestionSpecification {
-    //업무 분류(workYn)
+
     public static Specification<Question> equalNotWork(Integer cid){
         return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("cid"), cid));
     }
-    //삭제되지 않은 글(deleteYn=false)
-    public static Specification<Question> equalNotDeleted(Boolean deleteYn){
-        return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("deleteYn"), false));
-    }
+
     public static Specification<Question> notEqualNotWork(Integer cid){
         return ((root, query, criteriaBuilder) -> criteriaBuilder.notEqual(root.get("cid"), cid));
     }
-    //카테고리 검색(cid)
+
     public static Specification<Question> equalCid(Integer cid) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("cid"), cid);
     }
-    //사용자 검색(member)
+
     public static Specification<Question> equalWriterId(Long id) {
         return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("writerId"), id));
     }
-    //제목+내용 검색(titleAndContent)
+
     public static Specification<Question> equalTitleAndContentContaining(String value) {
         return (root, query, criteriaBuilder) -> {
             String pattern = "%" + value + "%";
@@ -34,5 +31,4 @@ public class QuestionSpecification {
             return criteriaBuilder.or(titleLike, contentLike);
         };
     }
-
 }
