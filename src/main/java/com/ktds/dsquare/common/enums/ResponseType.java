7a7933ba89cf0849c2716e52bb8c7cb1096bb8d1
이호열 <hoyeol.lee@kt.com>
2023-04-24 21:53:52 +0900

    FEAT: add global response type

---
+++ src/main/java/com/ktds/dsquare/common/enums/ResponseType.java
@@ -0,0 +1,37 @@
+package com.ktds.dsquare.common.enums;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import org.springframework.http.HttpStatus;
+
+@JsonFormat(shape = Shape.OBJECT)
+public enum ResponseType {
+
+    /*** Successful Response ***/
+    _200_OK(HttpStatus.OK, "200000", "OK."),
+
+    /*** Client Error Response ***/
+    _400_BAD_REQUEST(HttpStatus.BAD_REQUEST, "400000", "Bad request."),
+    _401_FAILED_LOGIN(HttpStatus.UNAUTHORIZED, "401001", "Login failed."),
+    _401_EXPIRED_CREDENTIALS(HttpStatus.UNAUTHORIZED, "401002", "Password is expired."),
+    _403_FORBIDDEN(HttpStatus.FORBIDDEN, "403000", "Forbidden."),
+    _403_LACK_OF_AUTHORITY(HttpStatus.FORBIDDEN, "403001", "You don't have an authority to access."),
+
+    /*** Server Error Response ***/
+    _500_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500000", "Internal server error."),
+
+    //
+    _999_JUNK_RESPONSE(HttpStatus.PROCESSING, "102999", "Junk response.")
+    ;
+
+    public final HttpStatus status;
+    public final String code;
+    public final String message;
+
+    ResponseType(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+}
