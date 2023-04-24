af7d0c17340433bff87ed43166caf58018b9d7a5
이호열 <hoyeol.lee@kt.com>
2023-04-24 22:43:00 +0900

    FEAT: add custom exception & handler
    - 추후 기존 exception handling 코드 정리 필요

--- src/main/java/com/ktds/dsquare/common/exception/DSquareExceptionHandler.java
+++ src/main/java/com/ktds/dsquare/common/exception/DSquareExceptionHandler.java
@@ -2,42 +2,37 @@ package com.ktds.dsquare.common.exception;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 @Slf4j
 public class DSquareExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler({DSquareRuntimeException.class})
-    public ResponseEntity<StatusMsg> dsquareExceptionHandler(HttpServletRequest request, HttpServletResponse respone, final DSquareRuntimeException e) {
-        e.printStackTrace();
-        log.error("##### DSquareExceptionHandler.DSquareRuntimeException: {}", e);
-
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST) //400
-                .body(StatusMsg.builder()
-                        .errorCode(e.getCode())
-                        .errorMsg(e.getMessage())
-                        .build());
-    }
-
-    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
-    public ResponseEntity<StatusMsg> dsquareExceptionHandler(HttpServletRequest request, HttpServletResponse respone, final Exception e) {
-        e.printStackTrace();
-        log.error("##### DSquareExceptionHandler.ExceptionHandler: ", e);
-
-        return ResponseEntity
-                .status(500)
-                .body(StatusMsg.builder()
-                        .errorCode(StatusEnum.UNKNOWN_ERROR.getCode())
-                        .errorMsg(StatusEnum.UNKNOWN_ERROR.getMessage())
-                        .build());
-    }
+//    @org.springframework.web.bind.annotation.ExceptionHandler({DSquareRuntimeException.class})
+//    public ResponseEntity<StatusMsg> dsquareExceptionHandler(HttpServletRequest request, HttpServletResponse respone, final DSquareRuntimeException e) {
+//        e.printStackTrace();
+//        log.error("##### DSquareExceptionHandler.DSquareRuntimeException: {}", e);
+//
+//        return ResponseEntity
+//                .status(HttpStatus.BAD_REQUEST) //400
+//                .body(StatusMsg.builder()
+//                        .errorCode(e.getCode())
+//                        .errorMsg(e.getMessage())
+//                        .build());
+//    }
+//
+//    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
+//    public ResponseEntity<StatusMsg> dsquareExceptionHandler(HttpServletRequest request, HttpServletResponse respone, final Exception e) {
+//        e.printStackTrace();
+//        log.error("##### DSquareExceptionHandler.ExceptionHandler: ", e);
+//
+//        return ResponseEntity
+//                .status(500)
+//                .body(StatusMsg.builder()
+//                        .errorCode(StatusEnum.UNKNOWN_ERROR.getCode())
+//                        .errorMsg(StatusEnum.UNKNOWN_ERROR.getMessage())
+//                        .build());
+//    }
 
 }
