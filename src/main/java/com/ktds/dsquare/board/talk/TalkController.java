09971f7002b32dbb66ececbd9913f51d87b1abee
NaKyoung-Kim <78343969+NaKyoung-Kim@users.noreply.github.com>
2023-04-27 11:18:59 +0900

    FIX: talk -> talks

--- src/main/java/com/ktds/dsquare/board/talk/TalkController.java
+++ src/main/java/com/ktds/dsquare/board/talk/TalkController.java
@@ -22,7 +22,7 @@ public class TalkController {
 
     // 소통해요 작성
     @ApiOperation(value="소통해요 작성", notes="소통해요 작성")
-    @PostMapping("/board/talk")
+    @PostMapping("/board/talks")
     public ResponseEntity<Void> createTalk(@RequestBody TalkRequest request, @AuthUser Member user){
         talkService.createTalk(request, user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -30,14 +30,14 @@ public class TalkController {
 
     // 소통해요 전체조회(정렬포함) + 검색 한번에
     @ApiOperation(value="소통해요 전체조회, 검색", notes="소통해요 전체조회, 검색")
-    @GetMapping("/board/talk")
+    @GetMapping("/board/talks")
     public ResponseEntity<List<BriefTalkResponse>> getTalks(@AuthUser Member user, @RequestParam(required = false) String key, @RequestParam(required = false) String value){
         return new ResponseEntity<>(talkService.getTalks(user, key, value), HttpStatus.OK);
     }
 
     // 소통해요 상세조회
     @ApiOperation(value="소통해요 상세조회", notes="소통해요 상세조회")
-    @GetMapping("/board/talk/{talkId}")
+    @GetMapping("/board/talks/{talkId}")
     public ResponseEntity<TalkResponse> getTalkDetail(
             @ApiParam(name="talkId", value="Talk Id", example = "1") @PathVariable Long talkId,
             @AuthUser Member user
@@ -47,7 +47,7 @@ public class TalkController {
 
     // 소통해요 수정
     @ApiOperation(value="소통해요 수정", notes="소통해요 수정")
-    @PostMapping("/board/talk/{talkId}")
+    @PostMapping("/board/talks/{talkId}")
     public ResponseEntity<Void> updateTalk(
             @ApiParam(name="talkId", value="Talk Id", example = "1") @PathVariable Long talkId,
             @RequestBody TalkRequest request){
@@ -57,7 +57,7 @@ public class TalkController {
 
     // 소통해요 삭제
     @ApiOperation(value="소통해요 삭제", notes="소통해요 삭제")
-    @DeleteMapping("/board/talk/{talkId}")
+    @DeleteMapping("/board/talks/{talkId}")
     public ResponseEntity<Void> deleteTalk(@ApiParam(name="talkId", value="Talk Id", example = "1") @PathVariable Long talkId) {
         talkService.deleteTalk(talkId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
