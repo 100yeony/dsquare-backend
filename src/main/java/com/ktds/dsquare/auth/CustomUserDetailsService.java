1ac6057405bbe9e874079e003f6176c3168e7990
이호열 <hoyeol.lee@kt.com>
2023-05-11 01:40:16 +0900

    FEAT: avoid lazy initialization

--- src/main/java/com/ktds/dsquare/auth/CustomUserDetailsService.java
+++ src/main/java/com/ktds/dsquare/auth/CustomUserDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +17,14 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
-    @Override
+    @Override @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.debug("loadUserByUsername ---------------------");
         log.debug("username : {}", username);
 
         Member member = memberRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("No such user with username [ " + username + "]"));
+        member.getRole().forEach(role -> {});
         return new CustomUserDetails(member);
     }
 
