7f1024b06a857ebf95306383a36858772945faad
YH <49009854+100yeony@users.noreply.github.com>
2023-05-15 15:05:20 +0900

    FIX: bulk data insert issue(generate ID)

--- src/main/java/com/ktds/dsquare/board/Post.java
+++ src/main/java/com/ktds/dsquare/board/Post.java
@@ -1,6 +1,7 @@
 package com.ktds.dsquare.board;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -8,12 +9,15 @@ import javax.persistence.*;
 @DiscriminatorColumn // (name = "DTYPE") // use default value
 @Inheritance(strategy = InheritanceType.JOINED)
 @Entity
+@Setter
 public class Post extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_sequence")
+    @SequenceGenerator(name = "post_sequence", sequenceName = "post_seq", initialValue = 100, allocationSize = 1)
     private Long id;
 
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    private Member writer;
 
 }
+
